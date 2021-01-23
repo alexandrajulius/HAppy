0709fcab5280a85c88b878093946b17875362d3a
alexandrajulius <alexandra.julius@gmail.com>
2021-01-23 22:35:53 +0100

    documents server creation

--- happy.py
+++ happy.py
@@ -3,10 +3,19 @@ import socketserver
 
 PORT = 8080
 
-# TCP Server get a TCP address (that is a tuple (ip address, port number))
-# and the Handler (that i have to implement)
-# (passing an empty string as the ip address means that the server
-# will listen on any network interface (all available IP addresses))
-httpd = socketserver.TCPServer(("", PORT), Happy_Handler)
-print("serving at port", PORT)
-httpd.serve_forever()
+try:
+    # Create an http server and define the handler
+    # to manage the incoming requests:
+    # 1. argument: a TCP address, that is a tuple (ip address, port number)
+    # (passing an empty string as the ip address means that the server
+    # will listen on any network interface (all available IP addresses))
+    # 2. argument: my HappyHandler
+    server = socketserver.TCPServer(('', PORT), Happy_Handler)
+    print('Started http server on port', PORT)
+
+    # Wait forever for incoming http requests
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print('^C received, shutting down the http server')
+    server.socket.close()
